8c00d63692b52d7d3651c5377456d20515df6a3c
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-09-11 07:13:12 +0200

    create update route & method for repetitions

--- app/controllers/repetitions_controller.rb
+++ app/controllers/repetitions_controller.rb
@@ -1,2 +1,13 @@
 class RepetitionsController < ApplicationController
+
+  def update
+    @repetition = Repetition.find(params[:id])
+    @repetition.update(repetition_params)
+  end
+
+  private
+
+  def repetition_param
+    params.require(:repetition).permit(:is_complete)
+  end
 end
