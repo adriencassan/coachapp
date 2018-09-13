b3c7527b20172e85cd6bf49face767e10d6bb3cd
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-09-13 09:56:44 +0200

    initate route and view for Workouts#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :workoutsets
+  resources :workouts, only: [:show]
   resources :repetitions, only: [:update]
 end
