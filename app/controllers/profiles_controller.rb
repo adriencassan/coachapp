a5200b3b997fdd8e35638020912715fa665eaab8
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-09-15 15:09:31 +0200

    commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,7 +7,8 @@ class ProfilesController < ApplicationController
 
   def coachee
     @profile = Profile.find(params[:profile_id])
-    @results = @profile.results.order(date: :desc).limit(5).map { |result| [result.date.strftime("%d/%m"), result.weight]}.reverse
+    @results = @profile.results
+    @results_graph = @results.order(date: :desc).limit(5).map { |result| [result.date.strftime("%d/%m"), result.weight]}.reverse
     @programs = Workout.where(profile: @profile, is_program: true)
     @workouts = Workout.where(profile: @profile, is_program: false).order(date: :desc).paginate(page: params[:page], per_page: 5)
   end
