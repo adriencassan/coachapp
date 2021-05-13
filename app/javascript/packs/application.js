6135ec69a9fcadede9f0fee9f6faa03d78112ab6
Adrien Cassan <adrien.cassan.tech@gmail.com>
2021-05-13 09:39:11 +0200

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,12 @@
-
 import "bootstrap";
-import "flatpickr";
-
-flatpickr(".datepicker", {});
 
 
 import { jsFormEditBtn, jsFormInputBtnPlus,jsFormInputBtnMinus, jsFormShowBtn } from "../jsFormEdit";
 import { initUpdateNavbarOnScroll} from "../navbar.js";
-
-
 
 jsFormEditBtn();
 jsFormInputBtnPlus();
 jsFormInputBtnMinus();
 jsFormShowBtn ();
 initUpdateNavbarOnScroll();
+
