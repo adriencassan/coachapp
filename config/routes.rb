8a564e75605b0814aa99db950fc5d09e6a856313
Adrien Cassan <adrien.cassan.tech@gmail.com>
2021-05-29 08:56:36 +0200

    commit"

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+
+
   resources :habitsets do
-     get ':id/destroy', to: "habitsets#destroy", as: "destroy"
+       get ':id/destroy', to: "habitsets#destroy", as: "destroy"
     get ':id/destroydate', to: "habitsets#destroydate", as: "destroydate"
     get ':id/destroystartdate', to: "habitsets#destroystartdate", as: "destroystartdate"
     get ':id/destroyreviewedat', to: "habitsets#destroyreviewedat", as: "destroyreviewedat"
