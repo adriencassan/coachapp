2196a85c5f56b84c06efe2c23cf61c98f2deba15
Adrien Cassan <adrien.cassan.tech@gmail.com>
2021-05-30 16:38:13 +0200

    commit

--- app/controllers/habitsets_controller.rb
+++ app/controllers/habitsets_controller.rb
@@ -4,7 +4,7 @@ class HabitsetsController < ApplicationController
   def index
     @habitsets_favorites = Habitset.where(is_favorite: true, is_completed: false).order(:date)
     @habitsets_upcoming = Habitset.where('date > ?', Date.today).order(:date)
-    @habitsets_failed = Habitset.where('date < ?', Date.today).where(status: "ongoing").order(:date)
+    @habitsets_failed = Habitset.where('date < ?', Date.today).where(status: "Ongoing").order(:date)
     @habitsets_inbox = Habitset.where(is_completed: false, is_favorite: false, date: nil).order(:date)
     @habitsets_today = Habitset.where(is_completed: false, is_favorite: false, date: Date.today).order(:date)
   end
